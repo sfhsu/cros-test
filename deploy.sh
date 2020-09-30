03224c0fb6ca7c0bd83751688619dc7492ac80ba
sfhsu <hunter.hsu@kneron.us>
2020-09-30 16:59:21 +0800

    Set env

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:sfhsu/cros-test.git master:gh-pages
 # 下面是走 ssh 模式
 # git push -f git@github.com:hsiangfeng/HexfootMusic.git master:gh-pages
 # 除此之外，也可以改走 HTTPS 模式
